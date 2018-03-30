8c3931f07bd30040db8bae9ab110b880e20a91e0
Renaud Chardin <chardin.r@sfeir.com>
2018-03-30 14:06:06 +0200

    Initial commit. PWA and firebase init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule} from './app-routing.module';
 
-import { ServiceWorkerModule } from '@angular/service-worker';
-import { AppComponent } from './app.component';
+import {ServiceWorkerModule} from '@angular/service-worker';
+import {AppComponent} from './app.component';
 
-import { environment } from '../environments/environment';
+import {environment} from '../environments/environment';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {UiModule} from './ui/ui.module';
+import {ContentModule} from './content/content.module';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    UiModule,
+    ContentModule,
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production })
+    BrowserAnimationsModule,
+    MatButtonModule, MatCheckboxModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {enabled: environment.production})
   ],
+  exports: [MatButtonModule, MatCheckboxModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
