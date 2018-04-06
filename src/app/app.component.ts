cea1e2aecaef66e82988708bffca6baea7bd440b
Renaud Chardin <chardin.r@sfeir.com>
2018-04-06 18:26:39 +0200

    test firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
-import { Observable } from 'rxjs/Observable';
+import {Component} from '@angular/core';
+import {AngularFirestore} from 'angularfire2/firestore';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +9,20 @@ import { Observable } from 'rxjs/Observable';
 })
 export class AppComponent {
   items: Observable<any[]>;
+  collabs: any[] = new Array<any>();
+
   constructor(db: AngularFirestore) {
+    const that = this;
     this.items = db.collection('items').valueChanges();
+    db.firestore.collection('collab').get().then(querySnapshot => {
+      querySnapshot.forEach(function(doc) {
+        // doc.data() is never undefined for query doc snapshots
+        console.log(doc.id, ' => ', doc.data());
+        that.collabs.push(doc.data());
+      });
+    });
     db.collection('items').add({
-      'test': (Math.random().toString(16).substr(2,8))
-    }).then(value => console.log(value));
+      'test': (Math.random().toString(16).substr(2, 8))
+    }).then(value => console.log('bla' + value));
   }
 }
