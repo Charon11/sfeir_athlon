ac74183f60ea8545224131ccf3357d1d08a5e1d8
Renaud Chardin <chardin.r@sfeir.com>
2018-04-26 14:04:49 +0200

    offline by firebase

--- src/app/services/team-leader.service.ts
+++ src/app/services/team-leader.service.ts
@@ -6,13 +6,18 @@ import {AngularFirestore} from 'angularfire2/firestore';
 @Injectable()
 export class TeamLeaderService {
 
+  private _db;
+
   constructor(private _fbDataBase: AngularFirestore) {
     const settings = {/* your settings... */ timestampsInSnapshots: true};
     this._fbDataBase.firestore.settings(settings);
+    this._fbDataBase.firestore.enablePersistence().then(this._db = this._fbDataBase.firestore);
   }
 
   get leaderboard(): Observable<Array<TeamLeader>> {
-    return Observable.fromPromise(this._fbDataBase.firestore.collection('classement').get().then(querySnapshot => {
+    return Observable.fromPromise(this._db.collection('classement').valueChanges()   .get().then(querySnapshot => {
+      const source = querySnapshot.metadata.fromCache ? 'local cache' : 'server';
+      console.log('Data came from ' + source);
       return querySnapshot.docs.map(d =>  d.data()).sort((a: TeamLeader, b: TeamLeader) => {
         if (a.classement < b.classement) {
           return -1;
