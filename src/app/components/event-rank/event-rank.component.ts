fbf547208c12ef87fd51d1fc454aa5b67362dd81
Renaud Chardin <chardin.r@sfeir.com>
2018-06-17 23:28:26 +0200

    displayName in event and photo

--- src/app/components/event-rank/event-rank.component.ts
+++ src/app/components/event-rank/event-rank.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {EventRank} from '../../models/event-rank';
 import {RankedTeamleader} from '../../models/ranked-teamleader';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-event-rank',
@@ -10,8 +11,9 @@ import {RankedTeamleader} from '../../models/ranked-teamleader';
 export class EventRankComponent implements OnInit {
 
   @Input('event-rank') private _eventRank: EventRank;
+  private _photoUrl;
 
-  constructor() {
+  constructor(private _sanitizer: DomSanitizer) {
   }
 
   ngOnInit() {
@@ -19,12 +21,18 @@ export class EventRankComponent implements OnInit {
       this._eventRank.teamleader = <RankedTeamleader>{
         lastname: doc.data().lastname,
         firstname: doc.data().firstname,
+        displayName: doc.data().displayName,
         photo: doc.data().photo
       };
+      this._photoUrl = this._sanitizer.bypassSecurityTrustStyle(`url(${this._eventRank.teamleader.photo})`);
     });
   }
 
   get eventRank(): EventRank {
     return this._eventRank;
+  }
+
+  get photoUrl() {
+    return this._photoUrl;
   }
 }
