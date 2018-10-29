3d542796b3d27c686f87136489c591653e43106b
Solene Schuster <solene.sch@gmail.com>
2018-10-29 09:49:31 +0100

    Mis en place de ChartJS

--- src/app/components/competitor/competitor.component.ts
+++ src/app/components/competitor/competitor.component.ts
@@ -3,6 +3,7 @@ import { TeamLeader } from '../../models/team-leader';
 import { DomSanitizer } from '@angular/platform-browser';
 import {RankedTeamleader} from '../../models/ranked-teamleader';
 import {EventsService} from '../../services/events.service';
+import {Observable} from 'rxjs/Observable';
 
 @Component({
   selector: 'app-competitor',
@@ -14,7 +15,9 @@ export class CompetitorComponent implements OnInit {
   @Input('teamleader') private _teamLeader: RankedTeamleader;
   private _photoUrl;
 
-  constructor(private _sanitizer: DomSanitizer) {
+  private addClassement: Array<Number> ;
+
+  constructor(private _sanitizer: DomSanitizer, private _eventsService: EventsService) {
   }
 
   get photoUrl() {
@@ -25,12 +28,33 @@ export class CompetitorComponent implements OnInit {
     return this._teamLeader;
   }
 
+  chartOptions = {
+    responsive: true
+  };
+
+  chartData = [
+    { data: [330, 600, 260, 700], label: 'Account A' },
+  ];
+
+  chartLabels = ['January', 'February', 'Mars', 'April'];
+
+  onChartClick(event) {
+    console.log(event);
+  }
+
   ngOnInit() {
     this._teamLeader.teamleader.onSnapshot(doc => {
       this._teamLeader.lastname = doc.data().lastname;
       this._teamLeader.firstname = doc.data().firstname;
       this._teamLeader.photo = doc.data().photo;
       this._teamLeader.displayName = doc.data().displayName;
+
+      this._eventsService.getaddClassementByTL(this._teamLeader.displayName).subscribe(addClassment => {
+        this.addClassement = addClassment;
+      })
+      console.log(this._teamLeader.displayName);
+      console.log('classmenet');
+      console.log(this.addClassement);
       this._photoUrl = this._sanitizer.bypassSecurityTrustStyle(`url(${this._teamLeader.photo})`);
     });
   }
